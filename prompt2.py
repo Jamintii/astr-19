d5d0314e87edc4ddb9887e31bbd7e6a6f3f2b0c8
Jamintii <146883052+Jamintii@users.noreply.github.com>
2023-10-15 15:07:54 -0700

    Add files via upload
    Session 2 prompt for Coding Journal 1

---
+++ prompt2.py
@@ -0,0 +1,39 @@
+#sum of floating numbers............
+
+#variables and inputs
+flo1 = float(input("Enter the first decimal:"))
+
+flo2 = float(input("Enter the second decimal:"))
+
+flosum = flo1 + flo2
+
+#printing results
+print("The sum of these two decimals is", flosum)
+print("The data type for the resulting answer is", type(flosum), "\n")
+
+#difference of integers.............
+
+#variables adn inputs
+dif1 = int(input("Enter the first number:"))
+
+dif2 = int(input("Enter the second number you're going to subract the first by:"))
+
+dif3 = dif1 - dif2
+
+#printing results
+print("The difference between these two numbers is:", dif3)
+print("The data type for the resulting answer is", type(dif3), "\n")
+
+#product of floating and integer.....
+
+#variables and inputs
+flonum1 = float(input("Enter a decimal:"))
+
+flonum2 = int(input("Enter a number:"))
+
+flonum3 = flo1 * flo2
+
+#printing results
+print("The product of this decimal and number is", flonum3)
+print("The data type for the resulting answer is", type(flonum3))
+
